56f00b067e62a8a0e2dcbd01ea18e8ebaf91c973
Isaiah <isaiahcvargas@gmail.com>
2024-04-15 00:50:27 -0400

    destroy action

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import ErrorPage from "./error-page";
 import Contact, {loader as contactLoader,} from "./routes/contact";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import EditContact,  { action as editAction,} from  "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+
 
 const router = createBrowserRouter([
   {
@@ -28,6 +30,10 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },
