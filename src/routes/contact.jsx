12a776cdc5ce53562d6379f123a5e95ca80585c6
Isaiah <isaiahcvargas@gmail.com>
2024-04-15 00:11:22 -0400

    URL params

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,7 +1,12 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
+
+export async function loader({ params }) {
+    const contact = await getContact(params.contactId);
+    return { contact };
 
 export default function Contact() {
-  const contact = {
+    const { contact } = useLoaderData();
     first: "Your",
     last: "Name",
     avatar: "https://placekitten.com/g/200/200",
